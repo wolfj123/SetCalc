11363f9ee7ab49d757052d6d49b4c4f1bb588ae6
wolf123 <wolfj@post.bgu.ac.il>
2017-04-26 17:59:11 +0300

    added unit test file for set

---
+++ src/setCalcTest/SetOperationsTest.java
@@ -0,0 +1,79 @@
+package setCalcTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SetOperationsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntersect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeepExistence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransformAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransformMul() {
+		fail("Not yet implemented");
+	}
+
+}
