cfec081fd263caef2fff48d0980bb27f4344fcf8
ariel <amsel@post.bgu.ac.il>
2017-04-26 18:01:04 +0300

    intersect

--- src/setCalc/Set.java
+++ src/setCalc/Set.java
@@ -10,7 +10,6 @@ class Set implements Element {
 		if (!_list.contains(e)) // check if an object already exists
 			_list.add(e);
 		return this;
-			
 	}
 	
 	public Set remove(Element e) {
@@ -19,8 +18,7 @@ class Set implements Element {
 	}
 	
 	public int size() {
-		// TODO Auto-generated method stub
-		return -1;
+		return _list.size();
 	}
 	
 	public Set union(Set s) {
@@ -31,8 +29,14 @@ class Set implements Element {
 	}
 	
 	public Set intersect(Set s) {
-		// TODO Auto-generated method stub
-		return null;
+
+		for (Element e : _list)
+		{
+			if (!s.member(e))
+				_list.remove(e);
+		}
+		
+		return this;
 	}
 	
 	public  Set difference(Set s) {
