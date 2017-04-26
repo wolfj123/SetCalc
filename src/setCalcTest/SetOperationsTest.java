50f6e1a1501fd99e7e3efbfd5cd9d01cdfe5266e
wolf123 <wolfj@post.bgu.ac.il>
2017-04-26 18:29:09 +0300

    SetOperationsTest : added insert test

--- src/setCalcTest/SetOperationsTest.java
+++ src/setCalcTest/SetOperationsTest.java
@@ -1,19 +1,31 @@
 package setCalcTest;
 
-import setCalc.Set;
+import setCalc.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SetOperationsTest {
-
+	private Set _set;
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		Set testSet = new Set()
+		_set = new Set();
+		Numeric[] numbers = new Numeric[]{
+			new Real(0),
+			new Real(1),
+			new Rational(1,2),
+			new Rational(2,4),
+			new Rational(5,1)
+		};
 		
-		
+		for (Numeric n : numbers){
+			_set.insert(n);
+		}
 	}
 
 	@After
@@ -22,7 +34,23 @@ public class SetOperationsTest {
 
 	@Test
 	public void testInsert() {
-		fail("Not yet implemented");
+		Numeric toInsert = new Rational(1,2);
+		_set.insert(toInsert);
+		
+		Set originalSet = new Set();
+		Numeric[] numbers = new Numeric[]{
+				new Real(0),
+				new Real(1),
+				new Rational(1,2),
+				new Rational(2,4),
+				new Rational(5,1)
+			};
+			
+			for (Numeric n : numbers){
+				_set.insert(n);
+			} 
+		
+		Assert.assertEquals("duplicate insert should not affect Set",_set.toString(),originalSet.toString() );
 	}
 
 	@Test
