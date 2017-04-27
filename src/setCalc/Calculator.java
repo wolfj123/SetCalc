a2bf46b27ec17b0a94b8de35b3fa28401e657af4
wolf123 <wolfj@post.bgu.ac.il>
2017-04-27 21:21:44 +0300

    Calculator : added switch cases

--- src/setCalc/Calculator.java
+++ src/setCalc/Calculator.java
@@ -15,6 +15,70 @@ public class Calculator {
 	public void calc(String instruction){
 		//StringTokenizer input = new StringTokenizer(instruction, " ");
 		String[] commands = instruction.trim().split("\\s+");
+		
+		switch(commands[0].toLowerCase()){
+		case "size":
+			break;
+			
+		case "contains":
+			break;
+			
+		case "member":
+			break;
+			
+		case "deepexistance":
+			break;
+			
+		case "equals":
+			break;
+			
+		case "insert":
+			break;
+			
+		case "remove":
+			break;
+			
+		case "union":
+			break;
+			
+		case "intersect":
+			break;
+			
+		case "difference":
+			break;
+			
+		case "power":
+			break;
+			
+		case "transformAdd":
+			break;
+			
+		case "transformMul":
+			break;
+			
+		case "help":
+			break;
+			
+		case "bonus":
+			break;
+			
+		case "exit":
+			break;
+			
+		default:
+			print("Unknown command");
+			break;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	private void print(String s){
+		System.out.println(s);
 	}
 	
 
