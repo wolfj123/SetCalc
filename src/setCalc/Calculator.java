e434118063ff8afa17930ff21761951a62ac2d76
wolf123 <wolfj@post.bgu.ac.il>
2017-04-27 13:41:19 +0300

    added Calculator.java and removed cli package

---
+++ src/setCalc/Calculator.java
@@ -0,0 +1,22 @@
+package setCalc;
+
+import java.util.StringTokenizer;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ask for user input and call calc(String instruction)
+
+	}
+	
+	
+	public void calc(String instruction){
+		StringTokenizer input = new StringTokenizer(instruction, " ");
+		
+		
+		
+	}
+
+}
