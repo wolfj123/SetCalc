79762af1a62b17ace621e1946c7f117412b3f5b7
ariel <amsel@post.bgu.ac.il>
2017-04-28 15:59:55 +0300

    finish tests

--- src/setCalc/Calculator.java
+++ src/setCalc/Calculator.java
@@ -1,11 +1,23 @@
 package setCalc;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
 //import java.util.StringTokenizer;
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Set _testSet = new Set (new Set());
+		System.out.println(_testSet);
+		Set _secondSet = new Set ((new Real(1)));
+		_secondSet.insert((new Real(2)));
+		System.out.println(_secondSet);
+		System.out.println("----------------");
+		System.out.println(_secondSet.union(_secondSet));
+		System.out.println(new Set().equals(new Real(1)));
+		System.out.println(_secondSet.power());
 		
 		//ask for user input and call calc(String instruction)
 
