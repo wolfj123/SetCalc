994d7a7101e45f7ab2fd3ce41f79fe5dc2122928
wolf123 <wolfj@post.bgu.ac.il>
2017-04-30 00:57:28 +0300

    fixed isValidString of set

--- src/setCalc/Calculator.java
+++ src/setCalc/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
 	public static String calc(String instruction){		
 		String[] commands = instruction.trim().split("\\s+");
 
-		String output = "Unknown command";
+		String output;
 		
 		
 		//attempt calling the method by name
@@ -38,7 +38,7 @@ public class Calculator {
 		}
 		catch (Exception e)
         {
-			return ("Unknown command" + e.getMessage());
+			return ("Unknown command");
         }
 		
 		return output;
@@ -205,16 +205,7 @@ public class Calculator {
 	}
 	
 	static private Element createElement(String s){
-		
 		return Set.createElementFromString(s);
-		/*
-		if(isNumeric(s))
-			return createNumeric(s);
-		if(isSet(s))
-			return createSet(s);
-	
-		return null;
-		*/
 	}
 	
 
